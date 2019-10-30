91fbb7246aa9b7a19993d163895f62c9b881ebb8
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-30 11:59:02 +0000

    data fetched from api

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react'
 
 class PokemonContainer extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      pokemonArray: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.pokemontcg.io/v1/cards')
+      .then(res => res.json())
+      .then(apidata => this.setState({pokemonArray: apidata.cards}))
+  }
 
   render(){
     return(
