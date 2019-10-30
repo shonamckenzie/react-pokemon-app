5c51711f3b2cb65890dc69cdd4361f7342d5a3be
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-30 11:40:15 +0000

    created pokemon container

---
+++ src/containers/PokemonContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class PokemonContainer extends Component {
+
+
+  render(){
+    return(
+      <h1>Pokemon Cards</h1>
+    )
+  }
+
+}
+
+
+
+export default PokemonContainer
